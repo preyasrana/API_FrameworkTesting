d384545ea1cb30be2e3bcb0d149779e4b67f5dec
preyas <preyas.rana@zymr.com>
2022-06-17 17:37:56 +0530

    test

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"c883a759-74f9-4664-8104-9ca60ca12692","feature":"Verify Place APIs call","scenario":"Verify if delete place functionality its working","start":1655466952629,"group":1,"content":"","tags":"@smoketest,","end":1655466955924,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"65c71b67-672c-47ea-87ab-76b7b7827bd8","feature":"Verify Place APIs call","scenario":"Verify if delete place functionality its working","start":1655467618106,"group":1,"content":"","tags":"@smoketest,","end":1655467621448,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
