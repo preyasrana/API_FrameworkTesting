d845d5c7ccb011c51c6dc76f3d98ed1f08ff84a3
preyas <preyas.rana@zymr.com>
2022-06-21 10:55:24 +0530

    test

--- src/test/java/StepDefinations/sample.java
+++ src/test/java/StepDefinations/sample.java
@@ -4,21 +4,12 @@ import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.sun.tools.jxc.ConfigReader;
-
-import POJO.Addplace;
-import POJO.location;
 import Resources.APIsList;
 import Resources.TestDataBuild;
 import Resources.Utils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -37,6 +28,7 @@ public class sample extends Utils {
 	static String isbnid;
 	static String userID;
 	static String token;
+	static String Assignbook_isbnid;
 	JsonPath json;
 
 	@Given("add place payload with {string} {string} {string}")
@@ -49,18 +41,15 @@ public class sample extends Utils {
 	@Given("add user with {string} {string}")
 	public void add_user_payload(String USRNAME, String PASSWORD) throws FileNotFoundException {
 
-		req = given().spec(basicauth_requestspecification())
-				.body(testdata.Create_user_payload(USRNAME, PASSWORD));
-	}
-	
-	
-	@Given("add user with username and password")
-	public void add_user_payload() throws FileNotFoundException {
-		
-    	req = given().spec(basicauth_requestspecification()).body(testdata.create_user());
-		
+		req = given().spec(basicauth_requestspecification()).body(testdata.Create_user_payload(USRNAME, PASSWORD));
 	}
 
+	@Given("add user with username and password")
+	public void add_user_payload() throws FileNotFoundException {
+
+		req = given().spec(basicauth_requestspecification()).body(testdata.create_user());
+
+	}
 
 	@When("user call {string} with {string} http request")
 	public void user_call_with_post_http_request(String resource, String method) {
@@ -120,82 +109,79 @@ public class sample extends Utils {
 		System.out.println("apiresource" + apiresource.getResource());
 
 		if (method.equalsIgnoreCase("get")) {
-			
+
 			res = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 			System.out.println("Response is " + res);
 
 			getresponse = given().spec(basicauth_requestspecification()).get(apiresource.getResource()).then().spec(res)
 					.extract().response();
-			//getresponse = response.when().get(apiresource.getResource()).then().spec(res).extract().response();
+			// getresponse =
+			// response.when().get(apiresource.getResource()).then().spec(res).extract().response();
 			System.out.println(getresponse);
 			// get isbn no
 			isbnid = getjsonpath(getresponse, "books[7].isbn");
 			System.out.println("isbnid is -->" + isbnid);
 
 		} else if (method.equalsIgnoreCase("post")) {
-			
+
 			res = new ResponseSpecBuilder().expectContentType(ContentType.JSON).build();
 			System.out.println("Response is " + res);
-			
+
 			getresponse = req.when().post(apiresource.getResource()).then().spec(res).extract().response();
 			System.out.println(getresponse);
-			
+
 			userID = getjsonpath(getresponse, "userID");
-		    System.out.println("userID is -->" + userID);
+			System.out.println("userID is -->" + userID);
 
 		}
 
 	}
-	
-	
+
 	@When("userare call {string} with {string} http request")
 	public void createtoken(String resource, String method) throws FileNotFoundException {
 
 		APIsList apiresource = APIsList.valueOf(resource);
 		System.out.println("apiresource" + apiresource.getResource());
-		
+
 		if (method.equalsIgnoreCase("post")) {
-			
+
 			res = new ResponseSpecBuilder().expectContentType(ContentType.JSON).build();
 			System.out.println("Response is " + res);
-			
+
 			getresponse = req.when().post(apiresource.getResource()).then().spec(res).extract().response();
 			System.out.println(getresponse);
-			
-			
+
 			token = getjsonpath(getresponse, "token");
-		    System.out.println("token is -->" + token);
+			System.out.println("token is -->" + token);
 		}
-	
+
 	}
-	
+
 	@Given("assignbooktoUSer")
 	public void assignbooktoUSer() throws FileNotFoundException {
 
-		req = given().spec(basicauth_requestspecification()).headers("Authorization","Bearer " + token).body(testdata.post_assignbook_touser(userID,isbnid));
+		req = given().spec(basicauth_requestspecification()).headers("Authorization", "Bearer " + token)
+				.body(testdata.post_assignbook_touser(userID, isbnid));
 	}
-	
+
 	@When("user shouldbe call {string} with {string} http request")
 	public void Booksapi(String resource, String method) throws FileNotFoundException {
-		
 
 		APIsList apiresource = APIsList.valueOf(resource);
 		System.out.println("apiresource" + apiresource.getResource());
-		
+
 		if (method.equalsIgnoreCase("post")) {
-			
+
 			res = new ResponseSpecBuilder().expectContentType(ContentType.JSON).build();
 			System.out.println("Response is " + res);
-			
-			
+
 			getresponse = req.when().post(apiresource.getResource()).then().spec(res).extract().response();
 			System.out.println(getresponse);
-			
-			isbnid = getjsonpath(getresponse, "books[0].isbn");
-		    System.out.println("isbnid is -->" +isbnid);
-		}
-		
-	}
 
+			Assignbook_isbnid = getjsonpath(getresponse, "books[0].isbn");
+			System.out.println("Assignbook_isbnid is -->" + Assignbook_isbnid);
+		}
+
+	}
 
 }
