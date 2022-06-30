e927dbc39081b0b3331eb7ed637b1f485b16a961
preyas <preyas.rana@zymr.com>
2022-06-30 09:42:19 +0530

    test

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/java/Feature/SpotifyAuth2.0.feature", glue = { "StepDefinations" }, monochrome = true, plugin = {
+@CucumberOptions(features = "src/test/java/Feature/BasicAuth_Bearer.feature", glue = { "StepDefinations" }, monochrome = true, plugin = {
 		"pretty", "html:target/Html-reports/cucumber-reports.html", "json:target/Json-reports/cucumber.json",
 		"junit:target/XML-reports/Cucumber.xml", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		"timeline:test-output-thread/" }, tags = "@smoketest or @Regression")
